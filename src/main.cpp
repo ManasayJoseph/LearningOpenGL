423eecd0a1645523788c26d927dd26a43b9ca222
ManasayJoseph <104974080+ManasayJoseph@users.noreply.github.com>
2024-05-01 15:24:17 +0530

    Learning-GlewToTry

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
@@ -7,7 +9,7 @@ int main(void)
     /* Initialize the library */
     if (!glfwInit())
         return -1;
-
+        //* Initializing glew
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window)
@@ -18,12 +20,21 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    std::cout << glewInit() << std::endl;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
+
+        //* Using legacy OpenGL to draw a triangle
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f,-0.5f);
+        glVertex2f(0.0f,0.5f);
+        glVertex2f(0.5f,-0.5f);
+        glEnd();
+
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
